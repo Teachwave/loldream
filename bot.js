ad34fae224fbfba4a279f4beb290894eb827fb46
Teachwave <52572229+Teachwave@users.noreply.github.com>
2019-08-19 11:59:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Iron Man (My DAD)`,"https://www.twitch.tv/Iron Man")
+client.user.setGame(`RIP (Rest In Peace)`,"https://www.twitch.tv/Iron Man")
 client.user.setStatus("dnd")
 });
 
